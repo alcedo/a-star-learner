bb4f610c2ff69e0377ccc726ff2d6b7f356f7ec5
konsolidate@gmail.com <konsolidate@gmail.com@18f9beca-e4b0-addf-238b-b0b75f15b33e>
2012-02-01 12:11:06 +0000

    GameScoring system added.

--- trunk/AStarLearner/AStarLearner/AStarLearner/GameScoringSystem.cs
+++ trunk/AStarLearner/AStarLearner/AStarLearner/GameScoringSystem.cs
@@ -5,8 +5,6 @@ using System.Text;
 
 namespace AStarLearner
 {
-
-
     class GameScoringSystem
     {
         /***************    Scoring Constants     ****************/
@@ -24,43 +22,54 @@ namespace AStarLearner
         private int totalScore = 0;
         // Keeps track of the previous game time in seconds, for time related bonus.
         private int previousGameTime = 0;
+       
+        // Record Current User session score 
+        private UserScoreRecords userRecords;
 
-        // Dictionary that maps username + userid to his score
-
-        // Write and save the score to a file. 
-
+        public void setUserSessionInfo(UserScoreRecords userRecords)
+        {
+            this.userRecords = userRecords;
+        }
+                
+        // Write and save the score to a file. (Use NLog for logging) 
+        
         // This function records the score of the user whenever a correct move is being made
         public void addScore(int gameTime)
         {
             int score = 0;  
+
             comboCount++;
 
             // Calculate how fast the user managed to get a correct answer. if < 3 seconds, award extra pts
             if (gameTime - previousGameTime < 3)  score = score + speedScore;
-            
-            if(comboCount == 3) 
 
-            // Tabulate final score 
+            if (comboCount > 3) score = score + comboCount;    // Combo score 
+
+            if (comboCount % 10 == 0) score = score + bonusScore;
+
+            totalScore += score; // Update overall score.
         }
 
         // Subtract scores from user when a wrong move is made. 
         public void decreaseScore()
         {
-            comboCount = 0;
+            // Reset combo counter 
+            comboCount = 0; 
+            totalScore -= baseScore;
         }
-
+        
         /// <summary>
         /// This class stores all relevant game score information
         /// </summary>
-        class UserScore
+        class UserScoreRecords
         {
-            private string name;            // User name 
-            private int userID;             // User ID 
-            private double score;           // Current On-going game score. Update high score if higher. 
-            private double highScore;       // Highest score achieve
-            private int highestCombo;       // Max combo scores achieved. 
-            private int maxLevel;           // Max level that the user has achieved 
-            private double gameDuration;    // Stores the duration played by user
+            public string name;            // User name 
+            public int userID;             // User ID 
+            public double score;           // Current On-going game score. Update high score if higher. 
+            public double highScore;       // Highest score achieve
+            public int highestCombo;       // Max combo scores achieved. 
+            public int maxLevel;           // Max level that the user has achieved 
+            public double gameDuration;    // Stores the duration played by user
 
         }
     }
