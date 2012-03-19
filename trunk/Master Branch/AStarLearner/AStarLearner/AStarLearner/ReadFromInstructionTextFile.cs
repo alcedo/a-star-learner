b9f2eee996a7a0b9d8e7d21f00b269943905ab8b
ryan.shijia@gmail.com <ryan.shijia@gmail.com@18f9beca-e4b0-addf-238b-b0b75f15b33e>
2012-03-19 07:51:10 +0000

    Added to files to be used later

---
+++ trunk/Master Branch/AStarLearner/AStarLearner/AStarLearner/ReadFromInstructionTextFile.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GameLevelManagement
+{
+    public class ReadFromInstructionTextFile
+    {
+        private string[] allTextInFile;
+        #region Constructor
+        public ReadFromInstructionTextFile()
+        {
+            //load the file from the directory
+            //Manual Documentation is required for the instructors to understand how the instruction being input 
+            this.allTextInFile = System.IO.File.ReadAllLines(@"C:\InstructionFolder\Instruction.txt");
+        }
+        #endregion
+
+        #region Methods
+        //given a level of the game, this will return a string back to the display
+        public string getLevelInstruction(int level)
+        {
+            int length = allTextInFile.Length;
+            string temp = allTextInFile[(length-1)-level];
+            int b = temp.IndexOf(':');
+            return temp.Remove(0,b+1);
+        }
+        #endregion
+    }
+
+
+}
